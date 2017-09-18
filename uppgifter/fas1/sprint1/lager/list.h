b9f03601d924f2245b9cd747855c68e3aa9827c8
Alexander T <mail@novium.pw>
2017-09-18 13:26:06 +0200

    Update list.h

--- uppgifter/fas1/sprint1/lager/list.h
+++ uppgifter/fas1/sprint1/lager/list.h
@@ -7,16 +7,16 @@
 
 #include <stdbool.h>
 
+/// Change this definition and replace int with the appropriate type
+/// in your program. 
+typedef int L;
+
 /// This function is used in list_delete() to allow the lists which are
 /// the sole owners of their data to free the data on delete.
 typedef void(*list_action)(L elem);
 
 /// Define struct list in your .c file not here! (why?)
 typedef struct list list_t;
-
-/// Change this definition and replace int with the appropriate type
-/// in your program. 
-typedef int L;
 
 /// \file list.h
 ///
